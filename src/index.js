00dde6cd7c3912a003817606fff665881f5692a1
Adarsh Pawar <66052094+Adarsh-Pawar@users.noreply.github.com>
2021-12-08 13:39:23 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
